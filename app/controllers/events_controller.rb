4550abbd4df5a4f41139667eb167b2221df18253
Michael <superinsky@gmail.com>
2012-01-20 14:47:44 +1100

    update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,12 @@
 class EventsController < ApplicationController
+	before_filter :find_event, :only => [:show, :edit, :update, :destroy]	
+
 	def index
-		@events = Event.all
+		@events = Event.page(params[:page]).per(5)
 	end
 
 	def show
-		@event = Event.find(params[:id])
+		@page_title = @event.name
 	end
 
 	def new
@@ -13,8 +15,45 @@ class EventsController < ApplicationController
 
 	def create
 		@event = Event.new(params[:event])
-		@event.save
+		flash[:notice] = "event was successfully created"
+		if @event.save
+			redirect_to :action => :index
+		else
+			render :action => :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		flash[:notice] = "event was successfully updated"
+		if @event.update_attributes(params[:event])
+			redirect_to :action => :show, :id =>@event
+		else
+			render :action =>  :edit
+		end
+	end
+
+	def destroy
+		@event.destroy
+		flash[:alert] = "event was successfully deleted"
 
 		redirect_to :action => :index
 	end
+
+	protected
+	
+	def find_event
+		@event = Event.find(params[:id])
+	end
+
+
+
+
+
+
+
+
+
 end
